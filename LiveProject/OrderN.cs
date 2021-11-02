a458efe5f87219201b01ae7c2d1010ca9667e27b
Mahafujul Alam Ansari <amahafujul44@gmail.com>
2021-11-02 20:04:22 +0530

    CC

--- LiveProject/OrderN.cs
+++ LiveProject/OrderN.cs
@@ -15,5 +15,25 @@ namespace LiveProject
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
