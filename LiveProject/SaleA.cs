85b7289166837b40c10726d9e36ca44247aaacff
Mahafujul Alam Ansari <alam.mahafujul@rediffmail.com>
2021-11-09 07:33:34 +0530

    good morning

--- LiveProject/SaleA.cs
+++ LiveProject/SaleA.cs
@@ -16,49 +16,9 @@ namespace LiveProject
             InitializeComponent();
         }
 
-        private void textBox9_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        
-
-        private void close_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void Sale_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Sale_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)
-            {
-                this.Close();
-            }
-            else if (e.KeyCode == Keys.Enter)
-            {
-                e.SuppressKeyPress = true;
-                SendKeys.Send("{TAB}");
-            }
-        }
-
-        private void groupBox7_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox4_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
